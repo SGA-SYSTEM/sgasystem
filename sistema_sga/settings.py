a15d9be7c10147c2d0e4a1db644accb6d6d88e3f
alexfalcucci <alex.falcucci@hotmail.com>
2014-07-10 21:28:05 -0300

    logout finished

--- sistema_sga/settings.py
+++ sistema_sga/settings.py
@@ -15,6 +15,12 @@ from dj_database_url import parse as db_url
 from unipath import Path
 BASE_DIR = Path(__file__).parent
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'slowtosay@gmail.com'
+EMAIL_HOST_PASSWORD = '007089'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
