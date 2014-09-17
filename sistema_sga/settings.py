b4f980a5ff45d925fd7b9107fe02cae5aaa73e3e
alexfalcucci <alex.falcucci@hotmail.com>
2014-09-17 18:18:41 -0300

    added flatpages

--- sistema_sga/settings.py
+++ sistema_sga/settings.py
@@ -67,6 +67,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.flatpages',
     # shell_plus
     'django_extensions',
     #debug-toolbar
