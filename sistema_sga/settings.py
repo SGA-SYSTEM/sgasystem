d945e5410c4f8d8d9d3be0c61fd1807116ffcb91
alexfalcucci <alex.falcucci@hotmail.com>
2014-09-18 18:41:36 -0300

    decouple

--- sistema_sga/settings.py
+++ sistema_sga/settings.py
@@ -182,11 +182,11 @@ MEDIA_ROOT = BASE_DIR.child('media')
 MEDIA_URL = '/media/'
 
 
-EMAIL_HOST='smtp.mandrillapp.com'
-EMAIL_HOST_USER='alex.falcucci@gmail.com'
-EMAIL_HOST_PASSWORD='eaERTcFdVL4YZ7MqZiCTdg'
-EMAIL_PORT=587
-EMAIL_USE_TLS=True
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False)
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
