4c92bdaf0c58ee69da961aa1b17416354eef65a8
alexfalcucci <alex.falcucci@hotmail.com>
2014-09-22 04:23:12 -0300

    issue bug #2

--- sistema_sga/prova/views.py
+++ sistema_sga/prova/views.py
@@ -94,7 +94,7 @@ def home_sga(request):
         query = UsuarioProva.objects.get(id=request.user.id)
         exams_pending = query.get_exams_pending()
     except :
-        exams_pending = ())
+        exams_pending = None
     context = {
     'usuario_prova_list': usuario_prova_list,
     'count_user': Profile.objects.all().count(),
